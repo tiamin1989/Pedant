5f160d4eb59940ba9b25fd989ce1eafb42134c53
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-03-06 20:24:13 +0500

    add: photos section

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,44 @@
 import Select from '../components/Select.js';
 
-const select = new Select('#select');
+function ready() {
+  const select = new Select('#select');
+  select.resizeSelect(this.selectedItem);
 
-document.getElementById('select').addEventListener(
-  'change',
-  function () { select.resizeSelect(this.selectedItem); },
-  false
-);
+  document.getElementById('select').addEventListener(
+    'change',
+    function () { select.resizeSelect(this.selectedItem); },
+    false
+  );
 
-document.getElementById('select').addEventListener(
-  'blur',
-  function () { select.resizeSelect(this.selectedItem); },
-  false
-);
+  document.getElementById('select').addEventListener(
+    'blur',
+    function () { select.resizeSelect(this.selectedItem); },
+    false
+  );
 
-document.getElementById('select').addEventListener(
-  'focus',
-  function () { select.restoreSelect(); },
-  false
-);
+  document.getElementById('select').addEventListener(
+    'focus',
+    function () { select.restoreSelect(); },
+    false
+  );
 
-document.querySelector('.header__hamburger').addEventListener(
-  'click',
-  function() {
-    document.querySelector('.header__hamburger-nav-list').style.visibility = 'visible';
-    document.querySelector('.header__hamburger-close').style.display = 'block';
-  },
-  false
-);
+  document.querySelector('.header__hamburger').addEventListener(
+    'click',
+    function() {
+      document.querySelector('.header__hamburger-nav-list').style.visibility = 'visible';
+      document.querySelector('.header__hamburger-close').style.display = 'block';
+    },
+    false
+  );
 
-document.querySelector('.header__hamburger-close').addEventListener(
-  'click',
-  function() {
-    document.querySelector('.header__hamburger-nav-list').style.visibility = 'hidden';
-    document.querySelector('.header__hamburger-close').style.display = 'none';
-  },
-  false
-);
+  document.querySelector('.header__hamburger-close').addEventListener(
+    'click',
+    function() {
+      document.querySelector('.header__hamburger-nav-list').style.visibility = 'hidden';
+      document.querySelector('.header__hamburger-close').style.display = 'none';
+    },
+    false
+  );
+}
+
+document.addEventListener("DOMContentLoaded", ready);
